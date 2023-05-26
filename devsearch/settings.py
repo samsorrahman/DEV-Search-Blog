9f834d1baad40e19259244542636ed22bb1a03ae
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-26 17:15:27 +0500

    static files installation started

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -44,12 +44,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'devsearch.urls'
--- projects/forms.py
+++ projects/forms.py
@@ -5,4 +5,5 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'featured_image', 'description',
+                  'demo_link', 'source_link', 'tags']
