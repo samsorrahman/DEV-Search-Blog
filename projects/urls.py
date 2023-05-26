6c7ce3c30fd5125988a7c88ecdd436539a628cd3
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-26 11:16:30 +0500

    Edit functionality added

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
     path('create-prject/', views.createProject, name='create-project'),
+    path('update-project/<str:pk>/', views.updateProject, name="update-project"),
 
 ]
