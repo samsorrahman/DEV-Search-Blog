df6a48f9023fa5fda340bdf46a5e013bf5809954
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-26 09:52:44 +0500

    Review and tag model added

--- projects/models.py
+++ projects/models.py
@@ -12,12 +12,39 @@ class Project(models.Model):
     #     null=True, blank=True, default="default.jpg")
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
-    # tags = models.ManyToManyField('Tag', blank=True)
-    # vote_total = models.IntegerField(default=0, null=True, blank=True)
-    # vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up vote'),
+        ('down', 'Down vote')
+    )
+    # owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
