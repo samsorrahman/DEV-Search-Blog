5d67185e4b99a0898eee1fd368ae3f801fff017b
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-26 09:08:05 +0500

    template and its inheritance

--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
