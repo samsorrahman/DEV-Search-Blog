572eb80e70a8c2280a897544ba7c7b1eaa8d1ac2
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-27 14:00:34 +0500

    login functionality added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from .models import Profile
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('username does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('username or password is incorrect')
+    return render(request, 'users/login_register.html')
 
 
 def profiles(request):
