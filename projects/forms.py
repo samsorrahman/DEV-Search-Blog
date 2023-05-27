1c67c111fccddac6c735b59a278c5713aa5b40b0
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-27 06:09:15 +0500

    add project template form designed

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 
 class ProjectForm(ModelForm):
@@ -7,3 +8,14 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'featured_image', 'description',
                   'demo_link', 'source_link', 'tags']
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__()
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
