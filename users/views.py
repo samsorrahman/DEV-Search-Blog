7c9ab89e041157dce97d98928dcf22989ded2f8d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-28 08:02:48 +0500

    account setting done

--- users/views.py
+++ users/views.py
@@ -82,3 +82,16 @@ def userProfile(request, pk):
 
     }
     return render(request, 'users/user-profile.html', context)
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    context = {
+        'profile': profile,
+        'skills': skills,
+        'projects': projects,
+    }
+    return render(request, 'users/account.html', context)
