09a4b09d97e2fc1b423f96856b0aeaafdc72a45c
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-29 20:09:29 +0500

    email sending funtionality added

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # @receiver(post_save, sender=Profile)
@@ -13,6 +15,27 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+
+        subject = 'Welcome to DevSearch '
+        message = 'We are glad you are here! <br><br>'
+        message += '<a href="https://www.linkedin.com/in/samsor-rahman18/">Connect with CEO on Linkedin!</a>'
+
+        # send_mail(
+        #     subject,
+        #     message,
+        #     settings.EMAIL_HOST_USER,
+        #     [profile.email],
+        #     fail_silently=False
+
+        # )
+        send_mail(
+            subject,
+            '',
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            html_message=message,
+            fail_silently=False
         )
 
 
