21bb46b697c7f7f0fa9b7ee20685557c90ea696d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-29 20:57:13 +0500

    email designed

--- users/signals.py
+++ users/signals.py
@@ -17,18 +17,18 @@ def createProfile(sender, instance, created, **kwargs):
             name=user.first_name,
         )
 
-        subject = 'Welcome to DevSearch '
-        message = 'We are glad you are here! <br><br>'
-        message += '<a href="https://www.linkedin.com/in/samsor-rahman18/">Connect with CEO on Linkedin!</a>'
+        subject = 'Welcome to DevSearch'
+        message = f"Dear {profile.name},\n\nWe are delighted to welcome you to DevSearch! It's a pleasure to have you on board.\n\n"
 
-        # send_mail(
-        #     subject,
-        #     message,
-        #     settings.EMAIL_HOST_USER,
-        #     [profile.email],
-        #     fail_silently=False
+        message += "You can connect with us on various social media platforms:\n>"
+        message += "- LinkedIn: <a href='https://www.linkedin.com/in/samsor-rahman18/'>CEO LinkedIn</a>\n>"
+        message += "- GitHub: <a href='https://github.com/samsorrahman'>CEO GitHub</a>\n"
+        message += "- Instagram: <a href='https://www.instagram.com/samsor_rahman'>CEO Instagram</a>\n"
+        message += "- Twitter: <a href='https://twitter.com/samsor_rahman'>CEO Twitter</a>\n\n"
 
-        # )
+        message += "If you have any questions or need assistance, feel free to reach out to us.<br><br>"
+        message += "Once again, welcome to DevSearch, and we wish you all the best in your journey!\n\n"
+        message += "Best regards,\nDevSearch Team"
         send_mail(
             subject,
             '',
