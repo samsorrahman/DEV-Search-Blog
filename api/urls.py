c5b46e8496a9df8a6ddceddc73bff4067a25d365
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-30 08:10:13 +0500

    getting single project

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('projects/', views.getProjects)
+    path('projects/', views.getProjects),
+    path('projects/<str:pk>/', views.getProject),
+
 ]
