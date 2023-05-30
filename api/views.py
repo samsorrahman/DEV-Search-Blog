759c52c466b27d2d101e023055a8483aeee444d9
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-30 07:55:08 +0500

    apis setting restframework installed

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': 'api/users/token'},
+        {'POST': 'api/users/token/refresh'},
+    ]
+
+    return Response(routes)
