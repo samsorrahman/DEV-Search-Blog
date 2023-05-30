9bf49375a24eba077248465c06409d6f4e9660e3
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-30 10:30:28 +0500

    customizing jwt in setting

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -56,8 +56,8 @@ REST_FRAMEWORK = {
 ...
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
