f756d80651ca75e4a9a7dc5d36cf4c6e410585e1
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-30 10:17:56 +0500

    authentication with web token apis

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
@@ -19,6 +20,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
